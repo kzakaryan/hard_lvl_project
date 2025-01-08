289c2d0335cbef736325d6042362e753c12ceb79
kzakaryan <kzakaryan@griddynamics.com>
2025-01-09 02:19:55 +0400

    Logic Changed for Test

--- src/main/java/model/VariableStorage.java
+++ src/main/java/model/VariableStorage.java
@@ -1,7 +1,8 @@
 package model;
 
 import java.math.BigDecimal;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Responsible for storing and managing variables used in calculations.
@@ -11,6 +12,16 @@ public class VariableStorage {
     private final Map<String, BigDecimal> variables = new HashMap<>();
 
     /**
+     * Retrieves the value of a stored variable.
+     *
+     * @param variableName the name of the variable.
+     * @return the value of the variable, or null if not found.
+     */
+    public BigDecimal getVariable(String variableName) {
+        return variables.get(variableName);
+    }
+
+    /**
      * Stores a variable and its corresponding value.
      *
      * @param variableName the name of the variable.
@@ -18,5 +29,15 @@ public class VariableStorage {
      */
     public void storeVariable(String variableName, BigDecimal value) {
         variables.put(variableName, value);
+    }
+
+    /**
+     * Checks if a variable exists in storage.
+     *
+     * @param variableName the name of the variable.
+     * @return true if the variable exists, otherwise false.
+     */
+    public boolean containsVariable(String variableName) {
+        return variables.containsKey(variableName);
     }
 }
